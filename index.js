11441743ac232d4d4ce805478fc4ef143ecb8a8f
kiran3453 <69121771+kiran3453@users.noreply.github.com>
2022-09-25 11:36:10 +0530

    prime number with boolan

--- index.js
+++ index.js
@@ -2,20 +2,38 @@ let userinput = document.querySelector(".input")
 let userinpt = document.querySelector(".inpt")
 let useroutput = document.querySelector(".output")
 let submit = document.querySelector("button")
-count =0 
-submit.onclick = function(){
-count = 0
-    for (let i =2; i < parseInt(userinpt.value); i++){
-        if (userinpt.value % i ==0){
-            count +=1
-            }
-        break;
-        }
-if (count==0){
-    useroutput.innerText = ("prime number")
-    }
-else{
-    useroutput.innerText = ("not prime number")
-    }
-}
 
+// submit.onclick = function(){
+// count = 0
+//     for (let i =2; i < parseInt(userinpt.value); i++){
+//         if (userinpt.value % i === 0){
+//             count +=1
+//             // break;
+//             }
+        
+//         }
+// if (count==0){
+//     useroutput.innerText = ("prime number")
+//     }
+// else{
+//     useroutput.innerText = ("not prime number")
+//     }
+// }
+
+submit.onclick = function(){
+    // count = 0
+    let con = false;
+        for (let i =2; i < parseInt(userinpt.value); i++){
+            if (userinpt.value % i === 0){
+                con = true;
+                break;
+                }
+            
+            }
+    if (con === false){
+        useroutput.innerText = ("prime number")
+        }
+    else{
+        useroutput.innerText = ("not prime number")
+        }
+    }
